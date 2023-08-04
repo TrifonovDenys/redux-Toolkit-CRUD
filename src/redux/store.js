a03052e1f456ce4496b7470a436e7bf14906df1e
Denys Tryfonov <trifonowden123@gmail.com>
2023-08-04 13:00:58 +0300

    add Slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { rootReducer } from './reduser';
-
-// Создаем расширение стора чтобы добавить инструменты разработчика
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+import { configureStore } from '@reduxjs/toolkit';
+// import { tasksReducer, filtersReducer } from './reducer';
+import { tasksReducer } from './tasksSlice';
+import { filtersReducer } from './filtersSlice';
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
